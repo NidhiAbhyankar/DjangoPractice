343c0d1408da749df53beb9a3a6573875cc09bef
NidhiAbhyankar <nidhiabhyankar@gmail.com>
2020-10-20 11:21:18 +0000

    remove old stuff

--- personal_portfolio_project/personal_portfolio/settings.py
+++ personal_portfolio_project/personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'sp)2gq-u2a%-r4j-!khmq%vn3v4=1tie1-qq*9wgk%m6zs659+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nidhimabhyankar.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= BASE_DIR / 'static'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
